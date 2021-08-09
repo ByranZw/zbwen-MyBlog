741a67611d7e5747d3881262470ea2c6442bfb25
zbwen <59199595+1065936727@users.noreply.github.com>
2021-08-09 15:29:40 +0800

    Update MailConfig.java

--- src/main/java/com/zbw/config/MailConfig.java
+++ src/main/java/com/zbw/config/MailConfig.java
@@ -2,7 +2,7 @@ package com.zbw.config;
 
 public class MailConfig {
 
-    public static final String MY_EMAIL = "15880609289@139.com";
+    public static final String MY_EMAIL = "XXX@XXX.com";
 
-    public static final String YT_EMAIL = "2285401037@qq.com";
+    public static final String YT_EMAIL = "XXX@XX.com";
 }
